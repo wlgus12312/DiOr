dda1d822b399fcb31d0c5ea91f914aa9a318584a
brain21c <brain21c@DESKTOP-S785EV3>
2021-02-18 17:10:55 +0900

    클래스 필드 게터세터삭제

--- DiOr/src/main/java/com/dior/food/dto/famFood.java
+++ DiOr/src/main/java/com/dior/food/dto/famFood.java
@@ -97,37 +97,6 @@ public class famFood {
 		super();
 	}
 
-	public long getNo() {
-		return no;
-	}
-
-	public void setNo(long no) {
-		this.no = no;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getYn() {
-		return yn;
-	}
-
-	public void setYn(String yn) {
-		this.yn = yn;
-	}
-
-	public String getToday() {
-		return today;
-	}
-
-	public void setToday(String today) {
-		this.today = today;
-	}
 
 	@Override
 	public String toString() {
