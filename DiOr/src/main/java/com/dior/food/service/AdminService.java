68e069c91d3065a22d10288e20306f8f23cc60ab
brain21c <brain21c@DESKTOP-S785EV3>
2021-02-19 09:54:08 +0900

    20210219

--- DiOr/src/main/java/com/dior/food/service/AdminService.java
+++ DiOr/src/main/java/com/dior/food/service/AdminService.java
@@ -7,7 +7,13 @@ import java.util.Map;
 import com.dior.food.dto.famFood;
 
 public interface AdminService{
-
-	ArrayList<famFood> getMenu() throws Exception;
-
+	ArrayList<famFood> getStore() throws Exception;
+	ArrayList<famFood> getFood() throws Exception;
+	ArrayList<famFood> getFood(Map menuMap) throws Exception;
+	int setStore_Ins(Map map) throws Exception;
+	int setStore_Upd(Map map) throws Exception;
+	int setMenu_Ins(Map map) throws Exception;
+	int setMenu_Upd(Map map) throws Exception;
+	int setMenu_Del(Map map) throws Exception;
+	
 }
