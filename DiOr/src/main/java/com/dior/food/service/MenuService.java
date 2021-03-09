0cd3054a937269ecf5ee56d8921bb7b5fade1cb9
dodut <dodut@DESKTOP-88ECKU0>
2021-03-09 11:37:55 +0900

    dao 소스코드 변경

--- DiOr/src/main/java/com/dior/food/service/MenuService.java
+++ DiOr/src/main/java/com/dior/food/service/MenuService.java
@@ -2,10 +2,13 @@ package com.dior.food.service;
 
 import java.util.ArrayList;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import com.dior.food.dto.menuDto;
 
 public interface MenuService{
-
-	ArrayList<menuDto> getMenu() throws Exception;
+	ArrayList<menuDto> getMenu(JSONObject jObect) throws Exception;
+	int insOrder(JSONArray jArr) throws Exception;
 	ArrayList<menuDto> getOrder(int ordNo) throws Exception;
 }
