dcc76a17555bb164951d1e754b9dd6dbf27c48e1
brain21c <brain21c@brainfood.com>
2021-03-23 11:07:16 +0900

    WAR

--- DiOr/src/main/java/com/dior/food/DiOrApplication.java
+++ DiOr/src/main/java/com/dior/food/DiOrApplication.java
@@ -2,11 +2,20 @@ package com.dior.food;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy
 @SpringBootApplication
 @Configuration
-public class DiOrApplication {
+public class DiOrApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DiOrApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiOrApplication.class, args);
